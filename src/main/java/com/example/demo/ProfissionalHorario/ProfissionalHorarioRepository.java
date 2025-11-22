a4fd51598ceb0adb4d3531cfe9f2d19955e638e1
Claudio <claudiomoraisbn@gmail.com>
2025-11-22 15:59:16 -0300

    faltou subir

--- src/main/java/com/example/demo/ProfissionalHorario/ProfissionalHorarioRepository.java
+++ src/main/java/com/example/demo/ProfissionalHorario/ProfissionalHorarioRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.ProfissionalHorario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
+import java.util.List;
 
 @Repository
 public interface ProfissionalHorarioRepository extends JpaRepository<ProfissionalHorarioVO, Long> {
@@ -15,5 +19,17 @@ public interface ProfissionalHorarioRepository extends JpaRepository<Profissiona
             LocalTime horaFinal
     );
 
+    ProfissionalHorarioVO findByProfissionalVO_IdAndDiaSemanaContaining(Long mProfissionalId, String mDayOfWeek);
+    List<ProfissionalHorarioVO> findByProfissionalVO_id(Long mId);
+
+    @Query(
+            value = """
+                SELECT *
+                FROM horario_profissional
+                WHERE (:mHorario BETWEEN hpr_hora_inicio and hpr_hora_final)
+                    """,
+            nativeQuery = true
+    )
+    public ProfissionalHorarioVO getByHorario(@Param("mHorario") @DateTimeFormat(pattern = "HH:mm") LocalTime mHorario);
 
 }
